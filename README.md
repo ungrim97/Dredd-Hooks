86be703d0908176af2ce970ca547c0d652e5e01a
Mike Eve <ungrim97@gmail.com>
2016-02-09 13:14:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Dredd::Hooks - It's new $module
+
+# SYNOPSIS
+
+    use Dredd::Hooks;
+
+# DESCRIPTION
+
+Dredd::Hooks is ...
+
+# LICENSE
+
+Copyright (C) Mike Eve.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mike Eve &lt;ungrim97@gmail.com>
---
+++ lib/Dredd/Hooks.pm
@@ -0,0 +1,39 @@
+package Dredd::Hooks;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dredd::Hooks - It's new $module
+
+=head1 SYNOPSIS
+
+    use Dredd::Hooks;
+
+=head1 DESCRIPTION
+
+Dredd::Hooks is ...
+
+=head1 LICENSE
+
+Copyright (C) Mike Eve.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mike Eve E<lt>ungrim97@gmail.comE<gt>
+
+=cut
+
