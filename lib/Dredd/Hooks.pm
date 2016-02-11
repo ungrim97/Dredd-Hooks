9ae53676758c58e410f69d939c67b5bae7b8f13f
Mike Eve <ungrim97@gmail.com>
2016-02-11 09:04:38 +0000

    Added hooks handlers

--- lib/Dredd/Hooks.pm
+++ lib/Dredd/Hooks.pm
@@ -1,11 +1,115 @@
 package Dredd::Hooks;
-use 5.008001;
-use strict;
-use warnings;
+
+use Moo;
 
 our $VERSION = "0.01";
 
+use Hash::Merge;
+use Types::Standard qw/HashRef/;
 
+has hooks => (
+    is => 'ro',
+    isa => HashRef
+);
+
+sub BUILDARGS {
+    my ($self, %args) = @_;
+
+    $args{hooks} = $self->_parse_hooks(delete $args{hook_files});
+    return \%args;
+}
+
+sub _parse_hooks {
+    my ($self, $hook_files) = @_;
+
+    return unless $hook_files
+    my $hooks;
+    for my $hook_file ($hook_files){
+        $hooks = Hash::Merge::merge($hooks, do $hook_file);
+    }
+
+    return $hooks;
+}
+
+sub before {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{before}{$transaction->name};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
+
+sub beforeAll {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{beforeAll};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
+
+sub beforeEach {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{beforeEach};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
+
+sub beforeEachValidation {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{beforeEachValidation};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
+
+sub beforeValidation {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{beforeValidation}{$transaction->name};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
+
+sub after {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{after}{$transaction->name};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
+
+sub afterEach {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{afterEach};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
+
+sub afterAll {
+    my ($self, $transaction) = @_;
+
+    my $hook = $self->hooks->{afterAll};
+
+    return unless $hook;
+
+    return $hook->($transaction);
+}
 
 1;
 __END__
